ad524a16ea98f9d77657c2f4d92c1a76929b85db
Clint <clint@bugs.com>
2026-05-20 21:25:20 -0400

    Add first Python script: hello_world.py

---
+++ projects/hello_world.py
@@ -0,0 +1,6 @@
+print("Hello World! 👋")
+print("I just made this on my Motorola phone using Termux!")
+print("My name is Clint (1bugsybugs)")
+
+name = input("What's your name? ")
+print(f"Nice to meet you, {name}!")
